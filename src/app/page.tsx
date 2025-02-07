92001f74a577cf3b914c93eccacba0d601c485d5
Dallas Klein <dallasjklein@gmail.com>
2025-02-07 15:29:26 -0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Feb7_LogicPuzzle from "./components/Feb7_LogicPuzzle";
 import Feb6_ImageGeneration from "./components/Feb6_ImageGeneration";
 
 export default function Home() {
+
+  const [showFeb6, setShowFeb6] = useState(false);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
@@ -12,7 +15,14 @@ export default function Home() {
   return (
     <>
       <Feb7_LogicPuzzle />
-      <Feb6_ImageGeneration />
+      {showFeb6 && <Feb6_ImageGeneration />}
+      {!showFeb6 && (
+        <div className="flex justify-center items-center min-h-[20vh] pt-20">
+          <button className="bg-gray-500 text-white px-4 py-2 rounded-md" onClick={() => setShowFeb6(true)}>
+            Show Feb 6
+          </button>
+        </div>
+      )}
     </>
   );
 }
