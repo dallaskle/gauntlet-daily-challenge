c048e3bdf9d5637360968b7d398d1d38bf87233d
Dallas Klein <dallasjklein@gmail.com>
2025-02-08 14:04:02 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Debug: Print Python version and its path
-python3 --version
-echo "Python3 found at: $(which python3)"
+python --version
+echo "Python found at: $(which python3)"
 
 # Install Python dependencies into the 'python_packages' directory
-python3 -m pip install -r requirements.txt --target python_packages
+python -m pip install -r requirements.txt --target python_packages
 
 # Build Next.js project
 next build
